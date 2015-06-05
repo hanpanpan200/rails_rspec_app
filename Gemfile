119fe623f22dd327a29b0100f849ed71b05bce84
fionhan <hanpanpan200@gmail.com>
2015-06-05 16:42:47 +0800

    init rspec enviorentment

--- Gemfile
+++ Gemfile
@@ -45,8 +45,10 @@ group :development, :test do
   gem 'factory_girl'
 end
 group :test do
+  gem 'minitest'
+  gem 'test-unit'
   gem "faker", "~> 1.1.2"
-  gem "capybara", "~> 2.1.0"
+  #gem "capybara", "~> 2.1.0"
   gem "database_cleaner", "~> 1.0.1"
   gem "launchy", "~> 2.3.0"
   gem "shoulda-matchers", "~> 2.2.0"
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,11 @@
+require 'minitest/autorun'
 require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with firstname, lastname and email"
+  it "is invalid without firstname"
+  it "is invalid without lastname"
+  it "is invalid without email"
+  it "is invalid with duplicate email address"
+  it "returns a cotact's full name as a string"
 end
