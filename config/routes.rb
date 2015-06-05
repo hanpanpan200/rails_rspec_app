bb58b6e93f920edb077433f7c490576cb9dd0c0b
fionhan <hanpanpan200@gmail.com>
2015-06-05 15:53:41 +0800

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  resources :news_releases
+
+  get 'signin', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  resources :users
+  resources :sessions
+
+  resources :contacts
+
+  root to: 'contacts#index'
+end
