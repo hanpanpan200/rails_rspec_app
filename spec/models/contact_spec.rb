4677387910d6213174e947ef5a9c01da7baac87d
fionhan <hanpanpan200@gmail.com>
2015-06-05 17:26:09 +0800

    add tests

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -2,8 +2,14 @@ require 'minitest/autorun'
 require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
-  it "is valid with firstname, lastname and email"
-  it "is invalid without firstname"
+  it "is valid with firstname, lastname, email and 3 phones" do
+    contact=Contact.new(firstname:'Tim',lastname:'Macor',email:'tim@gmail.com')
+    expect(contact).to  be_valid
+  end
+  it "is invalid without firstname" do
+    contact=Contact.new(firstname:nil)
+  end
+
   it "is invalid without lastname"
   it "is invalid without email"
   it "is invalid with duplicate email address"
